c5a879ad41124d2a541348ca14bd67128dd2ed47
Alex <go.fox.narod@gmail.com>
2024-07-09 15:24:57 +0300

    ----

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        my_first_function();
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        sum(x,y);
+    }
+    static void sum(int x, int y){
+        int z = x+y;
+        System.out.println(z);
+    }
 
-    }
-    static void  my_first_function(){
-        System.out.println("!!!!");
-    }
 }
