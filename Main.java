2a0a09e50e4f1e38807ad6097b9895c53e86a26b
Alex <go.fox.narod@gmail.com>
2024-07-09 15:09:03 +0300

    ---

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HW");
+        my_first_function();
+
+    }
+    static void  my_first_function(){
+        System.out.println("!!!!");
     }
 }
